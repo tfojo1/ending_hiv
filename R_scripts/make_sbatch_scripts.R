3c5d4f3ed653047d595ea3e05c2044c9113a3790
tfojo1 <anthony.fojo@jhmi.edu>
2021-06-30 14:11:39 -0400

    added default memory for quick run

--- R_scripts/make_sbatch_scripts.R
+++ R_scripts/make_sbatch_scripts.R
@@ -70,7 +70,7 @@ make.quick.run.scripts <- function(msa.indices,
                              chains=1:4,
                              dir='R_scripts/quick_run_scripts/',
                              account='tfojo1',
-                             mem=NULL)
+                             mem='1GB')
 {
     for (i in msa.indices)
     {
