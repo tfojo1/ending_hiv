5765de647145162291024d3db66fdf87fcb6bd9a
tfojo1 <anthony.fojo@jhmi.edu>
2021-07-16 07:48:32 -0400

    48h for intervention scripts

--- R_scripts/make_sbatch_scripts.R
+++ R_scripts/make_sbatch_scripts.R
@@ -163,7 +163,7 @@ make.intervention.scripts <- function(msa.indices,
                            mem=mem,
                            output = file.path(OUTPUT.DIR, paste0("int_", msa.name, ".out")),
                            partition = partition,
-                           time.hours = 12,
+                           time.hours = 48,
                            account=account,
                            commands= paste0("Rscript Ending_HIV/R_scripts/run_interventions_script.R ", i))
      }
