1ac857f2bc45401b30b44ec2b24d04dda3f42588
tfojo1 <anthony.fojo@jhmi.edu>
2021-07-18 09:03:06 -0400

    9600mbb for int scripts

--- R_scripts/make_sbatch_scripts.R
+++ R_scripts/make_sbatch_scripts.R
@@ -151,7 +151,7 @@ make.intervention.scripts <- function(msa.indices,
                                       dir='R_scripts/intervention_scripts/',
                                       partition='shared',
                                       account='tfojo1',
-                                      mem='9600GB')
+                                      mem='9600MB')
 {
     msa.indices = check.msa.indices(msa.indices)
     
