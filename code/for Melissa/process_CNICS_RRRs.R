fc55c1a7950bb0fbb7a1d9931dc00106a59d5710
mschnure <mschnur3@jhmi.edu>
2021-09-08 11:36:50 -0400

    updated cnics output

--- code/for Melissa/process_CNICS_RRRs.R
+++ code/for Melissa/process_CNICS_RRRs.R
@@ -1,4 +1,4 @@
-load("~/Dropbox/Documents_local/Hopkins/PhD/Dissertation/EHE/Ending_HIV/ending_hiv/code/for Melissa/CNICS analysis/multinomial_output_synthetic_2021-09-07")
+load("~/Dropbox/Documents_local/Hopkins/PhD/Dissertation/EHE/Ending_HIV/ending_hiv/code/for Melissa/CNICS analysis/multinomial_output_real_2021-09-07")
 
 ## Engaged unsuppressed
 covariates.EU = names(output$engaged.unsuppressed.coefficients[1:(length(output$engaged.unsuppressed.coefficients)/2)])
@@ -22,9 +22,9 @@ standard.error.ES = sqrt(diag(output$engaged.suppressed.variance))
 ci.upper.ES = exp(output$engaged.suppressed.coefficients - qnorm(.025)*standard.error.ES)
 ci.lower.ES = exp(output$engaged.suppressed.coefficients + qnorm(.025)*standard.error.ES)
 
-tab = paste0(format(exp(output$engaged.suppressed.coefficients), digits = 2, nsmall=2),
-             " [", format(ci.lower.ES, digits=2, nsmall=2),
-             " to ", format(ci.upper.ES, digit=2, nsmall=2), "]")
+tab = paste0(format(exp(output$engaged.suppressed.coefficients), digits = 1, nsmall=1),
+             " [", format(ci.lower.ES, digits=1, nsmall=1),
+             " to ", format(ci.upper.ES, digit=1, nsmall=1), "]")
 
 engaged.suppressed.table = matrix(tab, (length(output$engaged.suppressed.coefficients)/2),2, 
                                     dimnames = list(covariates.ES,future.states.ES))
@@ -38,9 +38,9 @@ standard.error.DE = sqrt(diag(output$disengaged.variance))
 ci.upper.DE = exp(output$disengaged.coefficients - qnorm(.025)*standard.error.DE)
 ci.lower.DE = exp(output$disengaged.coefficients + qnorm(.025)*standard.error.DE)
 
-tab = paste0(format(exp(output$disengaged.coefficients), digits = 2, nsmall=2),
-             " [", format(ci.lower.DE, digits=2, nsmall=2),
-             " to ", format(ci.upper.DE, digit=2, nsmall=2), "]")
+tab = paste0(format(exp(output$disengaged.coefficients), digits = 1, nsmall=1),
+             " [", format(ci.lower.DE, digits=1, nsmall=1),
+             " to ", format(ci.upper.DE, digit=1, nsmall=1), "]")
 
 disengaged.table = matrix(tab, (length(output$disengaged.coefficients)/2),2, 
                                   dimnames = list(covariates.DE,future.states.DE))
