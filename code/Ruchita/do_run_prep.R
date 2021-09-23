f749d75a4cd426d41c9e878eb377039f9d088884
Ruchita Balasubramanian <Ruchita@Ruchitas-MacBook-Pro.local>
2021-09-23 15:42:24 -0400

    Code to write tables to tab deliminated file

--- code/Ruchita/do_run_prep.R
+++ code/Ruchita/do_run_prep.R
@@ -12,6 +12,9 @@ table.v1 = make.prep.table(msas=TARGET.MSAS,
                            intervention.codes=INJ.PREP.INTERVENTION.CODES,
                            comparison.codes=ORAL.PREP.INTERVENTION.CODES,
                            raw.prep.results=prep.results, include.totals = F, stat = 'rel.diff')
+
+write.table(table.v1, "/Users/Ruchita/Documents/JHU/HIV Compartmental Model/ending_hiv/code/Ruchita/table.v1.txt",col.names = TRUE, row.names = TRUE, sep="\t")
+
 # All vs No Intervention
 table.v2 = make.prep.table(msas=TARGET.MSAS,
                            intervention.codes=STAGGERED.ORAL.INJ.PREP.CODES,
@@ -19,4 +22,5 @@ table.v2 = make.prep.table(msas=TARGET.MSAS,
                            raw.prep.results=prep.results, include.totals = F,
                            stat='rel.diff')
 
-write.table(mydata, "c:/mydata.txt", sep="\t")
+
+write.table(table.v2, "/Users/Ruchita/Documents/JHU/HIV Compartmental Model/ending_hiv/code/Ruchita/table.v2.txt",col.names = TRUE, row.names = TRUE, sep="\t")
