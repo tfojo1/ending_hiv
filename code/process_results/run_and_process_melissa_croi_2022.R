72b63fa915da3b67693ec90ba5d7197da2f11f23
mschnure <mschnur3@jhmi.edu>
2021-10-26 14:15:37 -0400

    CROI interventions

--- code/process_results/run_and_process_melissa_croi_2022.R
+++ code/process_results/run_and_process_melissa_croi_2022.R
@@ -22,10 +22,10 @@ if (1==2)
     
     location = BALTIMORE.MSA
     
-    intervention.code.table = matrix(c('noint','ybhmsm.l90', 'ybhmsm.l95', 'ybhmsm.r80', 'ybhmsm.r90', 'ybhmsm.a90', 'ybhmsm.a95', 'ybhmsm.c.low', 'ybhmsm.c.high',
-                                       'noint', 'msmidu.l90', 'msmidu.l95', 'msmidu.r80', 'msmidu.r90', 'msmidu.a90', 'msmidu.a95','msmidu.c.low', 'msmidu.c.high',
-                                       'noint', 'wholepop.l90', 'wholepop.l95', 'wholepop.r80', 'wholepop.r90', 'wholepop.a90', 'wholepop.a95', 'wholepop.c.low','wholepop.c.high'),
-                                     nrow = 3, ncol=9, byrow = T)
+    intervention.code.table = matrix(c('noint', 'ybhmsm.p10', 'ybhmsm.p25', 'ybhmsm.l90', 'ybhmsm.l95', 'ybhmsm.r80', 'ybhmsm.r90', 'ybhmsm.as90', 'ybhmsm.as95', 'ybhmsm.c.low', 'ybhmsm.c.high',
+                                       'noint', 'msmidu.p10', 'msmidu.p25', 'msmidu.l90', 'msmidu.l95', 'msmidu.r80', 'msmidu.r90', 'msmidu.as90', 'msmidu.as95','msmidu.c.low', 'msmidu.c.high',
+                                       'noint', 'wholepop.p10', 'wholepop.p25', 'wholepop.l90', 'wholepop.l95', 'wholepop.r80', 'wholepop.r90', 'wholepop.as90', 'wholepop.as95', 'wholepop.c.low','wholepop.c.high'),
+                                     nrow = 3, ncol=11, byrow = T)
     
     tab = get.estimates.for.interventions(intervention.codes=intervention.code.table,
                                           location=location,
