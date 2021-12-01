ad5ff8cfe06a0ffc7d073b87655fddaa6981785d
Ruchita Balasubramanian <Ruchita@Ruchitas-MacBook-Pro.local>
2021-12-01 11:41:39 -0500

    Updated scenarios after feedback

--- code/Ruchita/create_prep_interventions4.R
+++ code/Ruchita/create_prep_interventions4.R
@@ -79,13 +79,31 @@ create.prep.interventions.v2 <- function(start.year=2023,
     if (suffix != '' && substr(suffix, 1,1)!='_')
         suffix = paste0("_", suffix)
 
+    BASELINE.TO.INJ.COVERAGE = create.intervention.unit(type='prep',
+                                                        start.year=start.year,
+                                                        rates=expression(discontinuation.to.coverage.fraction(inj.vs.oral.discontinuation.rr * (1-oral.prep.persistence)) /
+                                                                           persistence.to.coverage.fraction(oral.prep.persistence)),
+                                                        years=implemented.year,
+                                                        apply.function='multiplier',
+                                                        allow.less.than.otherwise = T)
     
     
-#    PREP.PLUS.10.ORAL = create.intervention.unit(type='prep', start.year=start.year,
-#                                                 rates=expression(0.10 * persistence.to.coverage.fraction(oral.prep.persistence)),
-#                                                 years=implemented.year,
-#                                                 apply.function='additive',
-#                                                 max.rate = 1)
+    BASELINE.TO.50.50.COVERAGE = create.intervention.unit(type='prep',
+                                                          start.year=start.year,
+                                                          rates=expression((0.5 * discontinuation.to.coverage.fraction(inj.vs.oral.discontinuation.rr * (1-oral.prep.persistence)) +
+                                                                              0.5 * persistence.to.coverage.fraction(oral.prep.persistence)) /
+                                                                             persistence.to.coverage.fraction(oral.prep.persistence)
+                                                          ),
+                                                          years=implemented.year,
+                                                          apply.function='multiplier',
+                                                          allow.less.than.otherwise = T)
+    
+    
+    ADDITIONAL.PLUS.10.ORAL = create.intervention.unit(type='prep', start.year=start.year,
+                                              rates=expression(0.10 * persistence.to.coverage.fraction(oral.prep.persistence)),
+                                               years=implemented.year,
+                                                 apply.function='additive',
+                                                 max.rate = 1)
     
     ADDITIONAL.PLUS.10.INJ = create.intervention.unit(type='lai.prep', start.year=start.year,
                                                 rates=expression(0.10 * persistence.to.coverage.fraction(1-(1-oral.prep.persistence)*inj.vs.oral.discontinuation.rr)),
@@ -93,33 +111,55 @@ create.prep.interventions.v2 <- function(start.year=2023,
                                                 apply.function='additive',
                                                 max.rate = 1)
     
+    ADDITIONAL.PLUS.25.ORAL = create.intervention.unit(type='prep', start.year=start.year,
+                                                       rates=expression(0.25 * persistence.to.coverage.fraction(oral.prep.persistence)),
+                                                       years=implemented.year,
+                                                       apply.function='additive',
+                                                       max.rate = 1)
+    
+    ADDITIONAL.PLUS.25.INJ = create.intervention.unit(type='lai.prep', start.year=start.year,
+                                                      rates=expression(0.25 * persistence.to.coverage.fraction(1-(1-oral.prep.persistence)*inj.vs.oral.discontinuation.rr)),
+                                                      years=implemented.year,
+                                                      apply.function='additive',
+                                                      max.rate = 1)
+    
+    
+    ADDITIONAL.PLUS.35.ORAL = create.intervention.unit(type='prep', start.year=start.year,
+                                                       rates=expression(0.35 * persistence.to.coverage.fraction(oral.prep.persistence)),
+                                                       years=implemented.year,
+                                                       apply.function='additive',
+                                                       max.rate = 1)
+    
+    ADDITIONAL.PLUS.35.INJ = create.intervention.unit(type='lai.prep', start.year=start.year,
+                                                      rates=expression(0.35 * persistence.to.coverage.fraction(1-(1-oral.prep.persistence)*inj.vs.oral.discontinuation.rr)),
+                                                      years=implemented.year,
+                                                      apply.function='additive',
+                                                      max.rate = 1)
+    
+    ADDITIONAL.PLUS.5.ORAL.5.INJ = create.intervention.unit(type='lai.prep', start.year=start.year,
+                                                            rates=expression(0.10 * (
+                                                              0.5 * persistence.to.coverage.fraction(oral.prep.persistence) +
+                                                                0.5 * persistence.to.coverage.fraction(1-(1-oral.prep.persistence)*inj.vs.oral.discontinuation.rr
+                                                                ))),
+                                                            years=implemented.year,
+                                                            max.rate = 1)
+    
+    
+
+    
+    #efficacy
+    #double check
+    
     BASELINE.TO.ORAL.EFFICACY = create.intervention.unit(type='rr.prep', start.year=2000,
-                                                  rates='oral.prep.rr',
-                                                  years=2000.001,
-                                                  allow.less.than.otherwise = T)
-    
-    BASELINE.TO.INJ.EFFICACY = create.intervention.unit(type='rr.lai.prep', start.year=2000,
-                                                 rates=expression(oral.prep.rr * inj.vs.oral.hr),
-                                                 years=2000.001,
-                                                 allow.less.than.otherwise = T)
-    
-    # converting baseline oral to inj
-    
-    BASELINE.TO.INJ.EFFICACY = create.intervention.unit(type='rr.prep', 
+                                                         rates='oral.prep.rr',
+                                                         years=2000.001,
+                                                         allow.less.than.otherwise = T)
+    BASELINE.TO.INJ.EFFICACY = create.intervention.unit(type='rr.lai.prep', 
                                                             start.year=start.year,
                                                             rates=expression(oral.prep.rr * inj.vs.oral.hr),
                                                             years=implemented.year,
                                                             allow.less.than.otherwise = T)
-    
-    BASELINE.TO.INJ.COVERAGE = create.intervention.unit(type='prep',
-                                                            start.year=start.year,
-                                                            rates=expression(discontinuation.to.coverage.fraction(inj.vs.oral.discontinuation.rr * (1-oral.prep.persistence)) /
-                                                                                 persistence.to.coverage.fraction(oral.prep.persistence)),
-                                                            years=implemented.year,
-                                                            apply.function='multiplier',
-                                                            allow.less.than.otherwise = T)
 
-    #50-50
     
     BASELINE.TO.50.50.EFFICACY = create.intervention.unit(type='rr.prep', start.year=2000,
                                                               rates=c('oral.prep.rr',
@@ -135,33 +175,29 @@ create.prep.interventions.v2 <- function(start.year=2023,
                                                               years=2000.001,
                                                               allow.less.than.otherwise = T)
     
-    BASELINE.TO.50.50.COVERAGE = create.intervention.unit(type='prep',
-                                                            start.year=start.year,
-                                                            rates=expression((0.5 * discontinuation.to.coverage.fraction(inj.vs.oral.discontinuation.rr * (1-oral.prep.persistence)) +
-                                                                                  0.5 * persistence.to.coverage.fraction(oral.prep.persistence)) /
-                                                                                 persistence.to.coverage.fraction(oral.prep.persistence)
-                                                                             ),
-                                                            years=implemented.year,
-                                                            apply.function='multiplier',
+    ADDITIONAL.TO.INJ.EFFICACY = create.intervention.unit(type='rr.lai.prep', start.year=2000,
+                                                            rates=expression(oral.prep.rr * inj.vs.oral.hr * persistence.to.coverage.fraction(1 - (1-oral.prep.persistence) * inj.vs.oral.discontinuation.rr)),
+                                                            years=2000.001,
                                                             allow.less.than.otherwise = T)
     
-    ADDITIONAL.PLUS.5.ORAL.5.INJ = create.intervention.unit(type='lai.prep', start.year=start.year,
-                                                               rates=expression(0.10 * (
-                                                                   0.5 * persistence.to.coverage.fraction(oral.prep.persistence) +
-                                                                       0.5 * persistence.to.coverage.fraction(1-(1-oral.prep.persistence)*inj.vs.oral.discontinuation.rr
-                                                                       ))),
-                                                               years=implemented.year,
-                                                               max.rate = 1)
+    ADDITIONAL.TO.ORAL.EFFICACY = create.intervention.unit(type='rr.prep', start.year=2000,
+                                                            rates=expression(oral.prep.rr * persistence.to.coverage.fraction(oral.prep.persistence)),
+                                                            years=2000.001,
+                                                            allow.less.than.otherwise = T)
     
+    
+
+
     # Make the interventions
     
+    #BASELINE
     baseline.oral = create.intervention(ALL.MSM,
                                         BASELINE.TO.ORAL.EFFICACY,oral.prep.rr.dist,inj.vs.oral.hr.dist, oral.prep.persistence.dist, inj.vs.oral.discontinuation.rr.dist)
     INTERVENTION.MANAGER = register.intervention(baseline.oral, code=paste0('msm.baseline.oral', suffix),
                                                  name='Baseline Oral PrEP on MSM',
                                                  manager = INTERVENTION.MANAGER,
                                                  allow.intervention.multiple.names = T)
-    
+    #SCENARIO 1
     baseline.to.lai = create.intervention(ALL.MSM,
                                           BASELINE.TO.INJ.COVERAGE,
                                           BASELINE.TO.LAI.EFFICACY,oral.prep.rr.dist,inj.vs.oral.hr.dist, oral.prep.persistence.dist, inj.vs.oral.discontinuation.rr.dist)
@@ -170,7 +206,17 @@ create.prep.interventions.v2 <- function(start.year=2023,
                                                  manager = INTERVENTION.MANAGER,
                                                  allow.intervention.multiple.names = T)
     
+    #SCENARIO 2
+    oral.baseline.plus.10.oral = create.intervention(ALL.MSM,
+                                                    BASELINE.TO.ORAL.EFFICACY,
+                                                    ADDITIONAL.PLUS.10.ORAL,
+                                                    ADDITIONAL.TO.ORAL.EFFICACY,oral.prep.rr.dist,inj.vs.oral.hr.dist, oral.prep.persistence.dist, inj.vs.oral.discontinuation.rr.dist)
+    INTERVENTION.MANAGER = register.intervention(oral.baseline.plus.10.oral, code=paste0('msm.baseline.oral.plus.10.oral', suffix),
+                                                 name='Baseline Oral PrEP with 10% Oral uptake on MSM',
+                                                 manager = INTERVENTION.MANAGER,
+                                                 allow.intervention.multiple.names = T)
     
+    #SCENARIO 3
     oral.baseline.plus.10.lai = create.intervention(ALL.MSM,
                                                     BASELINE.TO.ORAL.EFFICACY,
                                                     ADDITIONAL.PLUS.10.INJ,
@@ -179,6 +225,51 @@ create.prep.interventions.v2 <- function(start.year=2023,
                                                  name='Baseline Oral PrEP with 10% LAI uptake on MSM',
                                                  manager = INTERVENTION.MANAGER,
                                                  allow.intervention.multiple.names = T)
+    
+    #SCENARIO 4
+    oral.baseline.plus.25.oral = create.intervention(ALL.MSM,
+                                                     BASELINE.TO.ORAL.EFFICACY,
+                                                     ADDITIONAL.PLUS.25.ORAL,
+                                                     ADDITIONAL.TO.ORAL.EFFICACY,oral.prep.rr.dist,inj.vs.oral.hr.dist, oral.prep.persistence.dist, inj.vs.oral.discontinuation.rr.dist)
+    INTERVENTION.MANAGER = register.intervention(oral.baseline.plus.25.oral, code=paste0('msm.baseline.oral.plus.25.oral', suffix),
+                                                 name='Baseline Oral PrEP with 25% Oral uptake on MSM',
+                                                 manager = INTERVENTION.MANAGER,
+                                                 allow.intervention.multiple.names = T)
+    
+    #SCENARIO 5
+    oral.baseline.plus.25.lai = create.intervention(ALL.MSM,
+                                                    BASELINE.TO.ORAL.EFFICACY,
+                                                    ADDITIONAL.PLUS.25.INJ,
+                                                    ADDITIONAL.TO.INJ.EFFICACY,oral.prep.rr.dist,inj.vs.oral.hr.dist, oral.prep.persistence.dist, inj.vs.oral.discontinuation.rr.dist)
+    INTERVENTION.MANAGER = register.intervention(oral.baseline.plus.25.lai, code=paste0('msm.baseline.oral.plus.25.lai', suffix),
+                                                 name='Baseline Oral PrEP with 25% LAI uptake on MSM',
+                                                 manager = INTERVENTION.MANAGER,
+                                                 allow.intervention.multiple.names = T)
+    
+    
+    #SCENARIO 6
+    oral.baseline.plus.35.oral = create.intervention(ALL.MSM,
+                                                     BASELINE.TO.ORAL.EFFICACY,
+                                                     ADDITIONAL.PLUS.35.ORAL,
+                                                     ADDITIONAL.TO.ORAL.EFFICACY,oral.prep.rr.dist,inj.vs.oral.hr.dist, oral.prep.persistence.dist, inj.vs.oral.discontinuation.rr.dist)
+    INTERVENTION.MANAGER = register.intervention(oral.baseline.plus.35.oral, code=paste0('msm.baseline.oral.plus.35.oral', suffix),
+                                                 name='Baseline Oral PrEP with 25% Oral uptake on MSM',
+                                                 manager = INTERVENTION.MANAGER,
+                                                 allow.intervention.multiple.names = T)
+    
+    #SCENARIO 7
+    oral.baseline.plus.35.lai = create.intervention(ALL.MSM,
+                                                    BASELINE.TO.ORAL.EFFICACY,
+                                                    ADDITIONAL.PLUS.35.INJ,
+                                                    ADDITIONAL.TO.INJ.EFFICACY,oral.prep.rr.dist,inj.vs.oral.hr.dist, oral.prep.persistence.dist, inj.vs.oral.discontinuation.rr.dist)
+    INTERVENTION.MANAGER = register.intervention(oral.baseline.plus.35.lai, code=paste0('msm.baseline.oral.plus.35.lai', suffix),
+                                                 name='Baseline Oral PrEP with 35% LAI uptake on MSM',
+                                                 manager = INTERVENTION.MANAGER,
+                                                 allow.intervention.multiple.names = T)
+    
+    
+    
+    #Additional
     
     lai.baseline.plus.10.lai = create.intervention(ALL.MSM,
                                                    BASELINE.TO.INJ.COVERAGE,
