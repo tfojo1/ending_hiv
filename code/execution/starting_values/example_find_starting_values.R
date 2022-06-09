8242e478d824542610b394540ddfeee2b78c853f
mschnure <mschnur3@jhmi.edu>
2022-06-09 10:52:37 -0400

    new LA starting values

--- code/execution/starting_values/example_find_starting_values.R
+++ code/execution/starting_values/example_find_starting_values.R
@@ -6,13 +6,22 @@ test.fn = create.test.starting.values.function(location=LA.MSA, version='expande
 
 # Original values 
 test = test.fn(lost.or.mult = 1, reengagement.or.mult=1, lost.or.slope.mult = 1)
+simplot(test$sim, facet.by='risk')
 simplot(test$sim, data.types=c('engagement','suppression','suppression.of.engaged','retention'))
+simplot(test$sim)
 
 # Increasing lost.or.mult increases proportion lost; increasing reengagement.or.mult increases re-engagement
-test = test.fn(lost.or.mult = 1.15, reengagement.or.mult=1.85, supp.or.slope.mult = 1.1) # time 0 2000; time 1 2010; time 2 2020
+test = test.fn(lost.or.mult = 1.15, 
+               reengagement.or.mult=1.85, 
+               supp.or.slope.mult = 1.1, het.trate.1.mult = 1.02,
+               het.trate.2.mult = 0.97, 
+               msm.trate.1.mult = 0.98, msm.trate.2.mult = 0.99) 
+# time 0 2000; time 1 2010; time 2 2020
 
-simplot(test$sim, data.types=c('engagement','suppression','suppression.of.engaged','retention'))
 simplot(test$sim, facet.by='risk')
+simplot(test$sim)
+simplot(test$sim, data.types=c('engagement','suppression','suppression.of.engaged','retention'))
+
 
 #saves in the ending hiv/start values/expanded 1.0 folder; don't push until I like the starting values
 save.starting.values(sim.and.params = test) 
@@ -27,4 +36,5 @@ save.starting.values(sim.and.params = test)
 # lost.or.mult = 1.1, reengagement.or.mult=1.5
 
 # LA
-# lost.or.mult = 1.15, reengagement.or.mult=1.85, supp.or.slope.mult = 1.1 
+# lost.or.mult = 1.15, reengagement.or.mult=1.85, supp.or.slope.mult = 1.1, 
+# het.trate.1.mult = 1.02,het.trate.2.mult = 0.97, msm.trate.1.mult = 0.98, msm.trate.2.mult = 0.99
