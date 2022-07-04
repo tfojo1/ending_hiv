ca9d48fcd6a69f976e9d889404182f0e96e3df5f
Ruchita Balasubramanian <Ruchita@Ruchitas-MacBook-Pro.local>
2022-07-04 10:05:19 -0400

    updated incidence tables and parameter distributions

--- code/applications/depression/depression_parameters.R
+++ code/applications/depression/depression_parameters.R
@@ -1,9 +1,9 @@
 dist = join.distributions(
-    suppression.untreated.vs.no.depression.or = Lognormal.Distribution(-.0834, .0193),
-    suppression.treated.vs.untreated.depression.or = Lognormal.Distribution(0, 1),
+    suppression.untreated.vs.no.depression.or = Lognormal.Distribution(.92, .73),
+    suppression.treated.vs.untreated.depression.or = Lognormal.Distribution(1.04, .0077),
     
-    lost.untreated.vs.no.depression.or = Lognormal.Distribution(0, 1),
-    lost.treated.vs.untreated.depression.or = Lognormal.Distribution(0, 1),
+    lost.untreated.vs.no.depression.or = Lognormal.Distribution(1.13, .32),
+    lost.treated.vs.untreated.depression.or = Lognormal.Distribution(.685, .025),
     
     depression.incidence.rr = Lognormal.Distribution(0, log(2)/2),
     depression.remission.rr = Lognormal.Distribution(0, log(2)/2),
@@ -22,14 +22,16 @@ dist = join.distributions(
     depression.treatment.initiation.hiv.vs.uninfected.rr = Lognormal.Distribution(0, log(2)/2),
     depression.treatment.discontinuation.hiv.vs.uninfected.rr = Lognormal.Distribution(0, log(2)/2),
     
-    sexual.susceptibility.untreated.vs.no.depression.rr = Lognormal.Distribution(0, 1),
-    sexual.susceptibility.treated.vs.untreated.depression.rr = Lognormal.Distribution(0, 1),
+    sexual.susceptibility.untreated.vs.no.depression.male.or = Lognormal.Distribution(.88, .107),
+    sexual.susceptibility.untreated.vs.no.depression.female.or = Lognormal.Distribution(.76, .077),
+    sexual.susceptibility.treated.vs.untreated.depression.or = Lognormal.Distribution(0, 1), #Could not find
     
-    idu.susceptibility.untreated.vs.no.depression.rr = Lognormal.Distribution(0, 1),
-    idu.susceptibility.treated.vs.untreated.depression.rr = Lognormal.Distribution(0, 1),
+    idu.susceptibility.untreated.vs.no.depression.male.rr = Lognormal.Distribution(.88, .107),
+    idu.susceptibility.untreated.vs.no.depression.female.rr = Lognormal.Distribution(.76, .077),
+    idu.susceptibility.treated.vs.untreated.depression.rr = Lognormal.Distribution(0, 1), #Could not find
     
-    testing.untreated.vs.no.depression.or = Lognormal.Distribution(0, 1),
-    testing.treated.vs.untreated.depression.or = Lognormal.Distribution(0, 1),
+    testing.untreated.vs.no.depression.or = Lognormal.Distribution(1.34, .1),
+    testing.treated.vs.untreated.depression.or = Lognormal.Distribution(0, 1), #Could not find
     
     idu.incidence.untreated.vs.no.depression.rr= Lognormal.Distribution(0, log(2)/2),
     idu.remission.untreated.vs.no.depression.rr= Lognormal.Distribution(0, log(2)/2),
