ed0cd0371feadd264d2d4f289c2a9bd28e9430c7
Ruchita Balasubramanian <Ruchita@Ruchitas-MacBook-Pro.local>
2022-07-06 14:31:24 -0400

    Fix local depression reading

--- code/applications/depression/read_local_depression_data.R
+++ code/applications/depression/read_local_depression_data.R
@@ -6,8 +6,7 @@
 # $prevalence
 # $prevalence.lower
 # $prevalence.upper
-
-#Example dir
+load("/Users/Ruchita/Documents/JHU/HIV Compartmental Model/ending_hiv/cached/DEFAULT.LOCALE.MAPPING.Rdata")
 dir = c("~/Documents/JHU/HIV Compartmental Model/Depression/map_data_2010_2012.csv","~/Documents/JHU/HIV Compartmental Model/Depression/map_data_2012_2014.csv","~/Documents/JHU/HIV Compartmental Model/Depression/map_data_2014_2016.csv","~/Documents/JHU/HIV Compartmental Model/Depression/map_data_2016_2018.csv")
 read.depression.prevalence <- function(dir)
 {
@@ -16,9 +15,11 @@ read.depression.prevalence <- function(dir)
   #Extract year and read csvs
   year = vector(length = length(dir))
   for (a in 1:length(dir)){
-    map_data[[a]] = read.csv(dir[a])
-    year[a] = map_data[[a]]$years[1]
+    map_data[[a]] = read.csv(dir[a])[-c(1:20),]
+    year[a] = as.character(map_data[[a]]$years[1])
   }
+  
+
   #Extract age 
   age = unique(map_data[[1]]$age_group)
   
@@ -28,18 +29,54 @@ read.depression.prevalence <- function(dir)
   geography = as.character(unique(map_data[[1]]$geography))
   rexp <- "^(\\w+)\\s?(.*)$"
   geography_split = cbind(sub(rexp,"\\1",geography),sub(rexp,"\\2",geography))
-  geography_split[,1] <-states[match(geography_split[,1],states[,1]),2]
-  geography_split = as.data.frame( geography_split)
+  geography_split_2nd = sub(rexp,"\\1",geography_split[,2])
+  geography_split_3rd = sub(rexp,"\\2",geography_split[,2])
+  
+  #For DC 
+  geography_split_DC = sub(rexp,"\\2",geography_split_3rd)
+  
+  #Fix multiword state names
+  geography_split[which(geography_split[,1] == "District"),1] <- "District of Columbia"
+  geography_split[which(geography_split[,1] == "District of Columbia"),2] <- geography_split_DC[which(geography_split[,1] == "District of Columbia")]
+  geography_split[which(geography_split[,1] == "West"),1] <- "West Virginia"
+  geography_split[which(geography_split[,1] == "West Virginia"),2] <- geography_split_3rd[which(geography_split[,1] == "West Virginia")]
+  geography_split[which(geography_split[,1] == "Rhode"),1] <- "Rhode Island"
+  geography_split[which(geography_split[,1] == "Rhode Island"),2] <- geography_split_3rd[which(geography_split[,1] == "Rhode Island")]
+  geography_split[which(geography_split_2nd == "Hampshire"),1] <- "New Hampshire"
+  geography_split[which(geography_split[,1] == "New Hampshire"),2] <- geography_split_3rd[which(geography_split[,1] == "New Hampshire")]
+  geography_split[which(geography_split_2nd == "Jersey"),1] <- "New Jersey"
+  geography_split[which(geography_split[,1] == "New Jersey"),2] <- geography_split_3rd[which(geography_split[,1] == "New Jersey")]
+  geography_split[which(geography_split_2nd == "York"),1] <- "New York"
+  geography_split[which(geography_split[,1] == "New York"),2] <- geography_split_3rd[which(geography_split[,1] == "New York")]
+  geography_split[which(geography_split_2nd == "Mexico"),1] <- "New Mexico"
+  geography_split[which(geography_split[,1] == "New Mexico"),2] <- geography_split_3rd[which(geography_split[,1] == "New Mexico")]
+  geography_split[intersect(which(geography_split_2nd == "Carolina"),which(geography_split[,1] == "North")),1] <- "North Carolina"
+  geography_split[which(geography_split[,1] == "North Carolina"),2] <- geography_split_3rd[which(geography_split[,1] == "North Carolina")]
+  geography_split[intersect(which(geography_split_2nd == "Carolina"),which(geography_split[,1] == "South")),1] <- "South Carolina"
+  geography_split[which(geography_split[,1] == "South Carolina"),2] <- geography_split_3rd[which(geography_split[,1] == "South Carolina")]
+  geography_split[intersect(which(geography_split_2nd == "Dakota"),which(geography_split[,1] == "North")),1] <- "North Dakota"
+  geography_split[which(geography_split[,1] == "North Dakota"),2] <- geography_split_3rd[which(geography_split[,1] == "North Dakota")]
+  geography_split[intersect(which(geography_split_2nd == "Dakota"),which(geography_split[,1] == "South")),1] <- "South Dakota"
+  geography_split[which(geography_split[,1] == "South Dakota"),2] <- geography_split_3rd[which(geography_split[,1] == "South Dakota")]
+
+  
+  geography_split[,1] <-state.name.to.abbreviation(geography_split[,1])
+  geography_split = as.data.frame(geography_split)
   geography_split <-geography_split %>% mutate_all(na_if,"")
-  geography_split = geography_split[!is.na(geography_split[,1]),]
+  
+  geography = geography[!is.na(geography_split[,2])]
   geography_split = geography_split[!is.na(geography_split[,2]),]
+  
   codes = state.and.region.name.to.nsduh.region.code(geography_split[,1],geography_split[,2]) #proper mapping to code? not all region codes split properly 
   
   #Create empty array 
-
-  
+  rv = list(
+    prevalence = array(0, dim = c(length(year),length(age),length(codes))),
+    prevalence.lower = array(0, dim = c(length(year),length(age),length(codes))),
+    prevalence.upper = array(0, dim = c(length(year),length(age),length(codes)))
+  )
   dim.names = list(year=year,
-                   geography = geography,
+                   codes = codes,
                    age=age) 
   dim(rv$prevalence) = sapply(dim.names, length)
   dimnames(rv$prevalence) = dim.names
@@ -49,10 +86,13 @@ read.depression.prevalence <- function(dir)
   dimnames(rv$prevalence.lower) = dim.names
   
   for(a in 1:length(year)){
-    for(b in 1:length(geography)){
+    map_data_yr = map_data[[a]]
+    for(b in 1:length(codes)){
       for(c in 1:length(age)){
-        matrix = map_data[which(map_data$geography == geography[a] && map_data$age_group == age[b]),]
-
+        matrix = map_data_yr[intersect(which(map_data_yr$age_group == age[c]),which(map_data_yr$geography == geography[b])),]
+        rv$prevalence[a,b,c] = matrix[,5]
+        rv$prevalence.lower[a,b,c] = matrix[,6]
+        rv$prevalence.upper[a,b,c] = matrix[,7]
       }
     }
     
