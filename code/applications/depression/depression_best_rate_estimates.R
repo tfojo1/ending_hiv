d2199661aec4b95716cb1cdb1b4da50ca31b6120
Ruchita Balasubramanian <Ruchita@Ruchitas-MacBook-Pro.local>
2022-07-07 10:44:23 -0400

    updated incidence

--- code/applications/depression/depression_best_rate_estimates.R
+++ code/applications/depression/depression_best_rate_estimates.R
@@ -6,37 +6,83 @@
 #       sex is: heterosexual_male, msm, female
 #       risk is: never_IDU, active_IDU, IDU_in_remission
 
-#figure out age, race, sex, risk lengths/breakdowns 
+#figure out age, race, sex, risk lengths/breakdowns, still need IDU status 
 get.estimated.depression.incidence <- function(version='expanded_1.0',
-                                               dir='code/applications/depression/',
+                                               dir='/code/applications/depression/',
                                                Prop.less.1.year = .71,
                                                MSM.Ratio.age1 = 2.142288557, 
                                                MSM.Ratio.age2 = 2.108974359)
 {
-    incidence_white = read.csv(paste0(dir,"White_Incidence.csv"))
-    incidence_black = read.csv(paste0(dir,"Black_Incidence.csv"))
-    incidence_hispanic = read.csv(paste0(dir,"Hispanic_Incidence.csv"))
+    incidence_white = read.csv(paste0(dir,"White_Incidence.csv")) #try to reconvert the actual incidence file instead for the proper age groups
+    incidence_black = read.csv(paste0(dir,"Black_Incidence.csv")) #read files correctly
+    incidence_hispanic = read.csv(paste0(dir,"Hispanic_Incidence.csv")) #initialize matrix correctly 
     rv = setup.depression.array.skeleton(version)
-    dim.names = list(age=age,
-                     race = race,
-                     sex=sex,
-                     risk=risk) 
-    dim(rv) = sapply(dim.names, length)
-    dimnames(rv) = dim.names
+   
+    #White incidence 
+    sapply(1:length(age), function(i){
+      sapply(1:length(sex), function(j){
+        if(j == 1){
+          rv[i,"other",j,] = -log(1-(incdence_white[i,j]*Prop.less.1.year))
+        } else if (j == 2){
+          if (i = 1){
+            rv[i,"other",j,] = -log(1-(incdence_white[i,1]*Prop.less.1.year)*MSM.Ratio.age1) 
+          } else{
+            rv[i,"other",j,] = -log(1-(incdence_white[i,1]*Prop.less.1.year)*MSM.Ratio.age2) 
+          }
+          
+        } else if (j == 3) {
+          rv[i,"other",j,] = -log(1-(incdence_white[i,j-1]*Prop.less.1.year))
+        }
+        
+        
+      })
+      
+    })
+    
+    #Black incidence 
+    sapply(1:length(age), function(i){
+      sapply(1:length(sex), function(j){
+        if(j == 1){
+          rv[i,"black",j,] = -log(1-(incdence_black[i,j]*Prop.less.1.year))
+        } else if (j == 2){
+          if (i = 1){
+            rv[i,"black",j,] = -log(1-(incdence_black[i,1]*Prop.less.1.year)*MSM.Ratio.age1) 
+          } else{
+            rv[i,"black",j,] = -log(1-(incdence_black[i,1]*Prop.less.1.year)*MSM.Ratio.age2) 
+          }
+          
+        } else if (j == 3) {
+          rv[i,"black",j,] = -log(1-(incdence_black[i,j-1]*Prop.less.1.year))
+        }
+        
+        
+      })
+      
+    })
+    
+    #Hispanic incidence 
+    sapply(1:length(age), function(i){
+      sapply(1:length(sex), function(j){
+        if(j == 1){
+          rv[i,"hispanic",j,] = -log(1-(incdence_hispanic[i,j]*Prop.less.1.year))
+        } else if (j == 2){
+          if (i = 1){
+            rv[i,"hispanic",j,] = -log(1-(incdence_hispanic[i,1]*Prop.less.1.year)*MSM.Ratio.age1) 
+          } else{
+            rv[i,"hispanic",j,] = -log(1-(incdence_hispanic[i,1]*Prop.less.1.year)*MSM.Ratio.age2) 
+          }
+          
+        } else if (j == 3) {
+          rv[i,"hispanic",j,] = -log(1-(incdence_hispanic[i,j-1]*Prop.less.1.year))
+        }
+        
+        
+      })
+      
+    })
     return(rv)
     
-<<<<<<< HEAD
-
-=======
-    age = '13-24 years'
-    race = 'black'
-    sex = 'heterosexual_male'
-    risk = 'never_IDU'
-    values = c(.1413, .1, .2)
-    
-    rv[age, race, sex, risk] = values[1]
-    
-    rv[, races, sex, risk]
+  
     
     # @Ruchita: populate this array
     
@@ -46,28 +92,16 @@ get.estimated.depression.incidence <- function(version='expanded_1.0',
     
     # Return
     rv
->>>>>>> 3a716bbcee27fd8084619f7d68a1b60955674107
 }
 
 get.estimated.depression.remission <- function(version='expanded_1.0',
                                                dir='code/applications/depression/')
 {
     rv = setup.depression.array.skeleton(version)
-<<<<<<< HEAD
     rv[] = .05
     return(rv)
-=======
     
-    # @Ruchita: populate this array
-    rv[] = 2
-    
-    # to load a file - called example.csv
-    # put it in the dir referenced above
-    # data = read.csv(file.path(dir, 'example.csv))
-    
-    # Return
-    rv
->>>>>>> 3a716bbcee27fd8084619f7d68a1b60955674107
+  
 }
 
 get.estimated.depression.treatment.initiation <- function(version='expanded_1.0',
