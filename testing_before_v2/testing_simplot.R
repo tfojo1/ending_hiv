794eb4ae8458648b91592b04d32bb5d198479870
Todd Fojo <tfojo1@jh.edu>
2023-09-08 10:16:40 -0400

    updates to testing

--- testing_before_v2/testing_simplot.R
+++ testing_before_v2/testing_simplot.R
@@ -1,11 +1,12 @@
 
 
 # The code for pulling up and saving the test sim
-if (!==2)
+if (1==2)
 {
   load('P:JHEEM/simulations/ehe/1.0_12580_full.Rdata')
   sim = simset@simulations[[1]]
-  save(sim, file='simulations/test_sim.Rdata')
+  sim2 = simset@simulations[[1000]]
+  save(sim, sim2, file='simulations/test_sim.Rdata')
 }
 
 source('code/source_code.R')
@@ -13,3 +14,4 @@ source('code/processing/visualization/sim_plots.R')
 
 load('simulations/test_sim.Rdata')
 simplot(sim)
+simplot(sim, sim2)
